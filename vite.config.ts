86fd6554f80e25475d19e17cd69080d94cfb5fe8
bjax20 <bdevangelista@up.edu.ph>
2026-05-20 21:57:02 +0800

    ts bugs

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,7 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import { wasp } from 'wasp/client/vite'
 
 export default defineConfig({
-  plugins: [
-    react(),
-    wasp(), // 🐝 Required by the modern Wasp compiler to build your app structure
-  ],
+  plugins: [],
   server: {
     open: true,
     allowedHosts: [
